cf64253f9f1877a3fa572805467d9c1046341753
dylanquaale <dylanquaale33@gmail.com>
2022-11-14 16:58:34 -0600

    got js to react to start button

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-var startQuiz = document.addElementById("start-button");
+var startQuiz = document.getElementById("start-button");
 
 startQuiz.addEventListener("click", function(){
     startQuiz();
-})
+
 
 function startQuiz(){
     console.log("start")
 }
+})
