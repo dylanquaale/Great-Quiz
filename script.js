e02456af748180c026ef3fe91ecedf6c987c52a8
dylanquaale <dylanquaale33@gmail.com>
2022-11-14 16:45:22 -0600

    started new java quiz take 3

---
+++ script.js
@@ -0,0 +1,9 @@
+var startQuiz = document.querySelector("#start-button");
+
+startQuiz.addEventListener("click", function(){
+    startQuiz();
+})
+
+function startQuiz(){
+    console.log("start")
+}
