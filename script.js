05b7cf7a0a412a45d1fe410fc64f919ed722057c
dylanquaale <dylanquaale33@gmail.com>
2022-11-15 08:13:31 -0600

    adding css styling

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
-var startQuiz = document.getElementById("start-button");
+var startButton = document.getElementById("start-button");
+// list of questions here
+// Timer? Start it up here
 
-startQuiz.addEventListener("click", function(){
+startButton.addEventListener("click", function(){
     startQuiz();
+});
 
+function startQuiz() {
+    console.log("start");
 
-function startQuiz(){
-    console.log("start")
-}
-})
+    // List of questions should exist
+
+    // LOOP?
+    // go to question 1
+    // Display question 1
+    // Wait for user to choose answer <- probably where you'll end up adding event listener(s)
+    
+    // if user chose correct answer
+        // Acknowledge correct answer (sound? display "correct!" message? Up to you)
+    
+        // Otherwise
+        // Acknowledge incorrect answer and punish by subtracting time from timer
+    // next question and repeat
+    // LOOP?
+
+    // after all questions are done
+        // send user to high scores page and ask them to type their name
+        // when submitted, 
+            // add name to localstorage
+            // dislpay list of high scores
+};
